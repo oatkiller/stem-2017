3e7235fc7d98a2e6accbfce885ace9f9ca28f7b7
oatkiller <raustin@endgame.com>
2017-11-20 20:52:50 -0500

    moved

--- week10/2/server.js
+++ week10/2/server.js
@@ -5,22 +5,20 @@ const port = 3000
 function handleRequest(request, response) {
   console.log('Method', request.method, 'url', request.url, 'connection', request.socket.remoteAddress)
 
-  // Content-Type does for HTTP what file extensions do for you operating system
-  // They tell the client what type of file it is receiving
-  response.setHeader('Content-Type', 'text/html')
+  if (request.method === 'GET') {
+    if (request.url === '/') {
+      // Content-Type does for HTTP what file extensions do for you operating system
+      // They tell the client what type of file it is receiving
+      response.setHeader('Content-Type', 'text/html')
 
-  if (request.method === 'POST') {
-    request.on(
-      'data',
-      function(data) {
-        console.log('receieved message:', data.toString())
-      }
-    )
-    response.write('<h1>Thanks for the message</h1>')
+      // Read index.html and send it to the client
+      fs.createReadStream('index.html').pipe(response)
+    } else if (request.url === '/cats.jpg') {
+
+      response.setHeader('Content-Type', 'image/jpeg')
+      fs.createReadStream('cats.jpg').pipe(response)
+    }
   }
-
-  // Read index.html and send it to the client
-  fs.createReadStream('index.html').pipe(response)
 }
 
 const server = http.createServer(handleRequest)
